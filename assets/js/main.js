93233ed17d814c4e1464d7e5849c0270cc2955e1
TaraRhoseyn <taragulwell@gmail.com>
2021-09-07 01:26:12 +0000

    feat: Add dark and light mode toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ function setActiveCls() {
 
 // Typewriter effect
 
-let typedText = 'CODING COMMUNICATOR';
+let typedText = 'THE CODING COMMUNICATOR';
 let currentText = '';
 let letter = '';
 let index = 0;
@@ -21,11 +21,39 @@ let index = 0;
 (function typewriterEffect() {
     currentText = typedText;
     letter = currentText.slice(0, ++index);
+    // $(".typing").textContent = letter;
     document.querySelector('.typing').textContent = letter;
     if(letter.length === currentText.length) {
-
+        // $(".typing").css("animation", "none");
     }
-    setTimeout(typewriterEffect, 200);
+    setTimeout(typewriterEffect, 100);
 }());
 
 
+// Dark and light mode
+
+modeSwitch = document.getElementById("modeSwitch");
+document.getElementById("modeSwitch").addEventListener("click", testMode)
+let modeSwitchColor = ""
+
+function testMode() {
+    modeSwitchColor = window.getComputedStyle(modeSwitch).backgroundColor;
+    console.log(modeSwitchColor);
+    if (modeSwitchColor === "rgb(204, 204, 204)") {
+        initDarkMode();
+    } else if (modeSwitchColor === "rgb(255, 136, 17)") {
+        initLightMode();
+    }
+}
+
+function initLightMode() {
+    $('body').css('backgroundColor', '#FFF8F0').css('color', '#392F5A');
+    $('.navbar').css('backgroundImage', 'linear-gradient(#FFF, #FFF8F0)');
+    $('.nav-link').css('color', '#392F5A');
+}
+
+function initDarkMode() {
+    $('body').css('backgroundColor', '#392F5A').css('color', '#FFF8F0');
+    $('.navbar').css('backgroundImage', 'linear-gradient(#000, #392F5A)');
+    $('.nav-link').css('color', '#FFF8F0');
+}
